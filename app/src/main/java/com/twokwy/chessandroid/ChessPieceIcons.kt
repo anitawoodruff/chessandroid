3d21853d579d5de7cd9025d31a27e73765948680
Anita <anita.woodruff@gmail.com>
2024-09-20 22:03:22 +0100

    Clean up ChessPieceIcons

--- app/src/main/java/com/twokwy/chessandroid/ChessPieceIcons.kt
+++ app/src/main/java/com/twokwy/chessandroid/ChessPieceIcons.kt
@@ -3,6 +3,7 @@ package com.twokwy.chessandroid
 import android.content.Context
 import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
+import com.twokwy.chessandroid.ChessPieceType.*
 import com.twokwy.chessandroid.PieceColor.BLACK
 import com.twokwy.chessandroid.PieceColor.WHITE
 
@@ -12,18 +13,18 @@ class ChessPieceIcons(private val context: Context) {
     private var whiteIcons = mutableMapOf<ChessPieceType, Drawable>()
 
     init {
-        addDrawable(ChessPiece(ChessPieceType.KING, WHITE), R.drawable.ic_king_white)
-        addDrawable(ChessPiece(ChessPieceType.QUEEN, WHITE), R.drawable.ic_queen_white)
-        addDrawable(ChessPiece(ChessPieceType.BISHOP, WHITE), R.drawable.ic_bishop_white)
-        addDrawable(ChessPiece(ChessPieceType.KNIGHT, WHITE), R.drawable.ic_knight_white)
-        addDrawable(ChessPiece(ChessPieceType.ROOK, WHITE), R.drawable.ic_rook_white);
-        addDrawable(ChessPiece(ChessPieceType.PAWN, WHITE), R.drawable.ic_pawn_white)
-        addDrawable(ChessPiece(ChessPieceType.KING, BLACK), R.drawable.ic_king_black)
-        addDrawable(ChessPiece(ChessPieceType.QUEEN, BLACK), R.drawable.ic_queen_black)
-        addDrawable(ChessPiece(ChessPieceType.BISHOP, BLACK), R.drawable.ic_bishop_black)
-        addDrawable(ChessPiece(ChessPieceType.KNIGHT, BLACK), R.drawable.ic_knight_black)
-        addDrawable(ChessPiece(ChessPieceType.ROOK, BLACK), R.drawable.ic_rook_black)
-        addDrawable(ChessPiece(ChessPieceType.PAWN, BLACK), R.drawable.ic_pawn_black)
+        addDrawable(ChessPiece(KING, WHITE), R.drawable.ic_king_white)
+        addDrawable(ChessPiece(QUEEN, WHITE), R.drawable.ic_queen_white)
+        addDrawable(ChessPiece(BISHOP, WHITE), R.drawable.ic_bishop_white)
+        addDrawable(ChessPiece(KNIGHT, WHITE), R.drawable.ic_knight_white)
+        addDrawable(ChessPiece(ROOK, WHITE), R.drawable.ic_rook_white);
+        addDrawable(ChessPiece(PAWN, WHITE), R.drawable.ic_pawn_white)
+        addDrawable(ChessPiece(KING, BLACK), R.drawable.ic_king_black)
+        addDrawable(ChessPiece(QUEEN, BLACK), R.drawable.ic_queen_black)
+        addDrawable(ChessPiece(BISHOP, BLACK), R.drawable.ic_bishop_black)
+        addDrawable(ChessPiece(KNIGHT, BLACK), R.drawable.ic_knight_black)
+        addDrawable(ChessPiece(ROOK, BLACK), R.drawable.ic_rook_black)
+        addDrawable(ChessPiece(PAWN, BLACK), R.drawable.ic_pawn_black)
     }
     private fun addDrawable(piece: ChessPiece, drawableResId: Int) {
         when (piece.color) {
