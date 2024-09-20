9401293ae64e72ca3a0168a74311eaded38702d3
Anita <anita.woodruff@gmail.com>
2024-09-20 16:05:32 +0100

    Convert ChessBoardView to a data class

--- app/src/main/java/com/twokwy/chessandroid/ChessSquare.kt
+++ app/src/main/java/com/twokwy/chessandroid/ChessSquare.kt
@@ -1,31 +1,19 @@
 package com.twokwy.chessandroid
 
 import android.graphics.Rect
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.RectShape
 import androidx.annotation.ColorInt
-import java.util.Objects
 import java.util.Optional
 
-class ChessSquare(private val location: String, val drawable: ShapeDrawable, var piece: Optional<ChessPiece>) {
+data class ChessSquare(private val location: String, val drawable: ShapeDrawable, var piece:
+Optional<ChessPiece>) {
 
     fun containsPoint(x: Float, y: Float): Boolean {
         val bounds = drawable.bounds
         val xInRange = x > bounds.left && x < bounds.right
         val yInRange = y > bounds.top && y < bounds.bottom
         return xInRange && yInRange
-    }
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as ChessSquare
-        return drawable == that.drawable && piece == that.piece
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(drawable, piece)
     }
 
     override fun toString(): String {
