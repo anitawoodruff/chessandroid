4365cb11f83ab90e8649a97af5cf5317e580b412
Anita W <1918555+anitawoodruff@users.noreply.github.com>
2024-09-20 22:51:25 +0100

    Piece taking (#3)
    * Refactor handleDrag a bit (and return false if dragging onto or off the board)
    
    * Only permit taking pieces of the opposite color

--- app/src/main/java/com/twokwy/chessandroid/ChessBoard.kt
+++ app/src/main/java/com/twokwy/chessandroid/ChessBoard.kt
@@ -26,18 +26,30 @@ class ChessBoard(private val squares: List<ChessSquare>, private val icons: Ches
         pieceDrawable.draw(canvas)
     }
 
-    fun handleDrag(downX: Float, downY: Float, upX: Float, upY: Float) : Boolean {
+    fun handleDrag(downX: Float, downY: Float, upX: Float, upY: Float): Boolean {
         val downSquare = getSquareContainingPoint(downX, downY)
         val upSquare = getSquareContainingPoint(upX, upY)
         Log.d("ChessBoard", String.format("downSquare=%s, upSquare=%s", downSquare, upSquare))
+        if (!downSquare.isPresent || !upSquare.isPresent) {
+            // Dragging onto or off of the board is not handled
+            return false
+        }
+        if (!downSquare.get().piece.isPresent) {
+            // Dragging from an empty square is not handled
+            return false
+        }
         if (downSquare == upSquare) {
+            // Tapping on a square is a no-op
             return true
         }
-        if (downSquare.isPresent && downSquare.get().piece.isPresent && upSquare.isPresent) {
-            movePiece(downSquare.get(), upSquare.get())
+        val pieceToBeMoved = downSquare.get().piece
+        val pieceToBeTaken = upSquare.get().piece
+        if (pieceToBeTaken.isPresent && pieceToBeMoved.get().color == pieceToBeTaken.get().color) {
+            // Attempting to take a piece of the same color is a no-op
             return true
         }
-        return false
+        movePiece(downSquare.get(), upSquare.get())
+        return true
     }
 
     private fun getSquareContainingPoint(
