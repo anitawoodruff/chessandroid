0bcc4ed348dec08a84c96859ff45d730c60ab535
Anita <anita.woodruff@gmail.com>
2024-09-21 16:39:23 +0100

    move location to its own file

--- app/src/main/java/com/twokwy/chessandroid/board/ChessSquare.kt
+++ app/src/main/java/com/twokwy/chessandroid/board/ChessSquare.kt
@@ -32,14 +32,3 @@ ChessPiece?, val drawable: ShapeDrawable) {
     }
 }
 
-data class Location(val x: Int, val y: Int) {
-
-    private val FILES = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
-    private val RANKS = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
-
-    override fun toString(): String {
-        val file = FILES[x]
-        val rank = RANKS[7 - y]
-        return "$file$rank"
-    }
-}
