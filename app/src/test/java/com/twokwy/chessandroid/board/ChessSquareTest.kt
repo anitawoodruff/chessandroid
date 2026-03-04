ee9612adc564428748c01cb2d4856b6f644127de
Anita <anita.woodruff@gmail.com>
2026-03-04 12:51:54 +0000

    Implement legal knight moves only

---
+++ app/src/test/java/com/twokwy/chessandroid/board/ChessSquareTest.kt
@@ -0,0 +1,10 @@
+package com.twokwy.chessandroid.board
+
+import org.junit.jupiter.api.Assertions.*
+
+class ChessSquareTest {
+
+    @org.junit.jupiter.api.Test
+    fun pieceCanReach() {
+    }
+}
---
+++ app/src/test/java/com/twokwy/chessandroid/pieces/ChessPieceTest.kt
@@ -0,0 +1,12 @@
+package com.twokwy.chessandroid.pieces
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class ChessPieceTest {
+
+    @Test
+    fun isLegalMove() {
+    }
+}
